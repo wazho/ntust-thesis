34b5feae10a56112f3dd526477ad55d8d7487b3e
Salmon <salmon.zh.tw@gmail.com>
2017-06-23 22:51:08 +0800

    Updated.

--- _Draft/Latex/build.bat
+++ _Draft/Latex/build.bat
@@ -4,13 +4,13 @@
 
 :: Close the PDF software
 taskkill /IM "PDFXEdit.exe"
-:: Remove olds
-del %output_path%%file_name%.pdf
 :: Output path
 set file_name=my_ntust_thesis
 set output_path=%~dp0
+:: Remove olds
+del %output_path%%file_name%.pdf
 :: Excute xelatex to generate the aux
-if exist %output_path%%file_name%.pdf xelatex .\%file_name%.tex -output-directory=%output_path%
+xelatex .\%file_name%.tex -output-directory=%output_path%
 :: Use bibtex with aux to create the bbl
 if exist %output_path%%file_name%.aux bibtex %output_path%%file_name%.aux
 :: Re-xelatex with bbl to create the references
